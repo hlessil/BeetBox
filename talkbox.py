d4e79dfcdd6ac65efc869aea459f8818a7b87a88
tkunic <tkunic@rocketmail.com>
2014-03-08 10:27:57 -0500

    adding female and male attendance soundsets, reorganizing sounds a bit, added alphabetic ordering of soundsets in talkbox.py

--- talkbox.py
+++ talkbox.py
@@ -42,6 +42,7 @@ def get_soundsets(soundset_dir):
 			soundconf = json.load(open(root + os.sep + CONF_JSON, 'r'))
 			soundconf['rootdir'] = root
 			soundsets.append(soundconf)
+	soundsets.sort()
 	return soundsets
 
 
